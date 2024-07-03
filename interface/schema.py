5e94970709eccfda828f7230f2cd9f65437819d0
luzy99 <zylu22@m.fudan.edu.cn>
2024-07-03 19:26:50 +0800

    update: requiements.txt

--- interface/schema.py
+++ interface/schema.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from enum import Enum
 import time
 from typing import List, Set, Generator, Optional
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic.v1 import BaseModel, Field
 
 
 class Issue(BaseModel):
